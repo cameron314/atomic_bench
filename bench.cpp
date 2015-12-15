b1c49245f7c1053f6360d67b22ee21f09c0e6175
Cameron <cameron@moodycamel.com>
2015-12-15 13:26:40 -0500

    Added benchmarks for different methods of obtaining thread IDs

--- bench.cpp
+++ bench.cpp
@@ -3,6 +3,23 @@
 #include <cstdio>
 #include <cstdlib>
 #include <atomic>
+#include <thread>
+#include <cstdint>
+
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <windows.h>
+#define THREAD_LOCAL __declspec(thread)
+#else
+#define THREAD_LOCAL thread_local
+#endif
+
+uintptr_t ghetto_thread_id()
+{
+	static THREAD_LOCAL int x;
+	return reinterpret_cast<uintptr_t>(&x);
+}
 
 int main(int argc, char** argv)
 {
@@ -47,6 +64,35 @@ int main(int argc, char** argv)
 			50
 		) * 1000 * 1000
 	);
+	
+	printf("std::this_thread::get_id() takes %.4fns on average\n",
+		moodycamel::microbench(
+			[&]() { std::this_thread::get_id(); },
+			10000,
+			50
+		) * 1000 * 1000
+	);
+	
+#ifdef _WIN32
+	printf("GetCurrentThreadId() takes %.4fns on average\n",
+		moodycamel::microbench(
+			[&]() { GetCurrentThreadId(); },
+			1000000,
+			50
+		) * 1000 * 1000
+	);
+#endif
+	
+#if !defined(__APPLE__) || (!defined(TARGET_IPHONE_SIMULATOR) && !defined(TARGET_OS_IPHONE))
+	volatile uintptr_t ghettoId = 0;
+	printf("ghetto_thread_id() takes %.4fns on average\n",
+		moodycamel::microbench(
+		[&]() { ghettoId += ghetto_thread_id(); },
+			1000000,
+			50
+		) * 1000 * 1000
+	);
+#endif
 	
 	return 0;
 }
